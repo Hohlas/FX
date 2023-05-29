0ebf0547d00a0de3b4b883a279c354e300860e1d
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2023-05-29 20:02:09 +0300

    Update csv_update.sh

--- MT_config/csv_update.sh
+++ MT_config/csv_update.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
+#!/bin/bash   
+# csv_update.sh  script to download #.csv files to all terminals
 source $HOME/.bashrc
 cd $GitFiles/FX && echo 'Download files from GitHub'
-git pull origin	main # забрать изменения
+git pull origin main --no-commit # забрать изменения
 SYMBOLS=(USDx DARW EUR USD) # названия папок/ярлыков   
 for index in ${!SYMBOLS[*]}
 do
 SYM=${SYMBOLS[$index]}
 # копирование настроек
-cp $MT$SYM/MQL4/Files/MatLab* $GitFiles/FX
+cp $MT$SYM/MQL4/Files/MatLab* $GitFiles/FX/MatLab$SYM".csv"
 cp $GitFiles/FX/'#.csv' $MT$SYM/MQL4/Files
 done
 cd $GitFiles/FX
