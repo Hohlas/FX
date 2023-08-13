edec20ad6ef5a9b75e8f136817bef01b637618b4
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2023-08-13 13:12:05 +0300

    Update csv_update.sh
    # script to copy MatLab.csv files to .mt4/GitFiles/FX
    # and update #.csv and experts files for all terminals

--- MT_config/csv_update.sh
+++ MT_config/csv_update.sh
@@ -1,14 +1,19 @@
 #!/bin/bash   
-# csv_update.sh  script to download #.csv files to all terminals
+# script to copy MatLab.csv files to .mt4/GitFiles/FX
+# and update #.csv and experts files for all terminals
 source $HOME/.bashrc
-cd $GitFiles/FX && echo 'Download files from GitHub'
+cd $GitFiles/FX && echo 'Download csv files from GitHub to .mt4/GitFiles/FX'
 git pull origin main --no-commit # забрать изменения
-SYMBOLS=(USDx DARW EUR USD) # названия папок/ярлыков   
-for index in ${!SYMBOLS[*]}
+cd $GitFiles/Fast20 && echo 'Download experts files from GitHub to .mt4/GitFiles/Fast20'
+git pull origin main --no-commit # забрать изменения
+
+TERMINAL_LIST=($SYMBOLS) # названия папок/ярлыков   
+for index in ${!TERMINAL_LIST[*]}
 do
-SYM=${SYMBOLS[$index]}
+SYM=${TERMINAL_LIST[$index]}
 # копирование настроек
-cp $MT$SYM/MQL4/Files/MatLab* $GitFiles/FX/MatLab$SYM".csv"
-cp $GitFiles/FX/'#.csv' $MT$SYM/MQL4/Files
+cp $MT$SYM/MQL4/Files/MatLab* $GitFiles/FX/MatLab$SYM".csv" && echo 'Download MatLab'$SYM'.csv to .mt4/GitFiles/FX'
+cp $GitFiles/FX/'#.csv' $MT$SYM/MQL4/Files && echo 'Download #.csv from .mt4/GitFiles to ..'$SYM'/MQL4/Files'
+cp -r $GitFiles/Fast20 $MT$SYM/MQL4 && echo 'Download experts files from .mt4/GitFiles to ..'$SYM'/MQL4/Experts'
 done
 cd $GitFiles/FX
