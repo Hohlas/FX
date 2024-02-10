e579f63d1c723caf816d16d40d004a5a7b73a9d2
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-02-10 19:34:02 +0300

    Update MT_Setup.sh

--- MT_config/MT_Setup.sh
+++ MT_config/MT_Setup.sh
@@ -3,28 +3,29 @@ SYMBOLS='USDx DARW EUR USD ROBO DEMO'
 echo "export SYMBOLS='"$SYMBOLS"'" >> $HOME/.bashrc
 echo 'export MT4="$HOME/.mt4/drive_c/Program Files (x86)/MetaTrader 4"' >> $HOME/.bashrc
 echo 'export MT=$HOME/.mt4/drive_c/' >> $HOME/.bashrc
-echo 'export GitFiles=$HOME/GitFiles' >> $HOME/.bashrc
-echo 'export GitFolder=$HOME/GitFiles/Fast20/.git/' >> $HOME/.bashrc
-echo 'export GitFx=https://raw.githubusercontent.com/Hohlas/FX/raw/main' >> $HOME/.bashrc 
+#echo 'export GitFiles=$HOME/GitFiles' >> $HOME/.bashrc
+#echo 'export GitFolder=$HOME/GitFiles/Fast20/.git/' >> $HOME/.bashrc
+#echo 'export GitFx=https://raw.githubusercontent.com/Hohlas/FX/raw/main' >> $HOME/.bashrc 
+
+if [ ! -d ~/Fast20 ]; then git clone https://github.com/Hohlas/Fast20.git ~/Fast20
+else 
+cd ~/Fast20; 
+git fetch origin; # get last updates from git
+git reset --hard origin/main # сбросить локальную ветку до последнего коммита из git
+fi 
 
 source $HOME/.bashrc
-mkdir -p $GitFiles; echo 'create GitFiles folder' ; 
-cd $GitFiles
-# curl $GitFx/MT_config/config/accounts.ini > ~/Desktop/tmp/accounts.ini
-echo 'copy files from GIT to GitFiles folder'
-cd $GitFiles && git clone https://github.com/Hohlas/FX.git
-cd $GitFiles &&  git clone https://github.com/Hohlas/Fast20.git
 
 rm -r "$MT4"/config/*
 rm -r "$MT4"/profiles/*
-cp -r $GitFiles/FX/MT_config/config/* "$MT4"/config/ && echo 'copy config files to '$MT4
-cp -r $GitFiles/FX/MT_config/profiles/* "$MT4"/profiles/ && echo 'copy profiles files to '$MT4
+cp -r ~/FX/MT_config/config/* "$MT4"/config/ && echo 'copy config files to '$MT4
+cp -r ~/FX/MT_config/profiles/* "$MT4"/profiles/ && echo 'copy profiles files to '$MT4
 # copy expetrs to MetaTrader4 dir
 rm -r "$MT4"/MQL4/Experts
 rm -r "$MT4"/MQL4/Include
 rm -r "$MT4"/MQL4/Indicators
 rm -r "$MT4"/MQL4/include
 rm -r "$MT4"/MQL4/indicators
-cp -r $GitFiles/Fast20/Experts "$MT4"/MQL4/
-cp -r $GitFiles/Fast20/include "$MT4"/MQL4/
-cp -r $GitFiles/Fast20/indicators "$MT4"/MQL4/
+cp -r ~Fast20/Experts "$MT4"/MQL4/
+cp -r ~Fast20/include "$MT4"/MQL4/
+cp -r ~Fast20/indicators "$MT4"/MQL4/
