fd68be49df96c578381464a6cde57a8526ca9392
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-02-10 19:11:38 +0300

    Update MT_Setup.sh

--- MT_config/MT_Setup.sh
+++ MT_config/MT_Setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-cd && rm -r Music Videos Pictures Public 
+SYMBOLS='USDx DARW EUR USD ROBO DEMO'
 echo "export SYMBOLS='"$SYMBOLS"'" >> $HOME/.bashrc
-echo 'export MT4=$HOME/.mt4/dosdevices/c:/Program\ Files/MetaTrader\ 4' >> $HOME/.bashrc
-echo 'export MT=$HOME/.mt4/dosdevices/c:/' >> $HOME/.bashrc
-echo 'export GitFiles=$HOME/.mt4/GitFiles' >> $HOME/.bashrc
+echo 'export MT4="$HOME/.mt4/drive_c/Program Files (x86)/MetaTrader 4"' >> $HOME/.bashrc
+echo 'export MT=$HOME/.mt4/drive_c/' >> $HOME/.bashrc
+echo 'export GitFiles=$HOME/GitFiles' >> $HOME/.bashrc
+echo 'export GitFolder=$HOME/GitFiles/Fast20/.git/' >> $HOME/.bashrc
 echo 'export GitFx=https://raw.githubusercontent.com/Hohlas/FX/raw/main' >> $HOME/.bashrc 
+
 source $HOME/.bashrc
 mkdir -p $GitFiles; echo 'create GitFiles folder' ; 
 cd $GitFiles
@@ -26,33 +28,3 @@ rm -r "$MT4"/MQL4/indicators
 cp -r $GitFiles/Fast20/Experts "$MT4"/MQL4/
 cp -r $GitFiles/Fast20/include "$MT4"/MQL4/
 cp -r $GitFiles/Fast20/indicators "$MT4"/MQL4/
-# copy 'MetaTrader4' dir to 'USD,EUR...' dirs
-TERMINAL_LIST=($SYMBOLS) # названия папок/ярлыков   
-for index in ${!TERMINAL_LIST[*]}
-do
-SYM=${TERMINAL_LIST[$index]}
-if [ -d $MT$SYM ]; then 
-echo 'Hey, terminal ' $MT$SYM ' already exist'
-continue 
-fi
-# if [ ! -d $MT$SYM ]; then 
-mkdir -p $MT$SYM; echo 'create folder ' $SYM ; 
-# копирование папок с терминалом
-echo 'copy files to folder ' $SYM ;
-cp -r $HOME/.mt4/dosdevices/c:/Program\ Files/MetaTrader\ 4/* $MT$SYM
-
-# Создание ярлыков запуска терминалов на рабочем столе
-cat > $HOME/Desktop/$SYM.desktop <<EOF
-[Desktop Entry]
-Name=$SYM
-Exec=env WINEPREFIX="$HOME/.mt4" wine-stable C:\\\\\\\\windows\\\\\\\\command\\\\\\\\start.exe /Unix $MT$SYM/terminal.exe
-Type=Application
-StartupNotify=true
-Path=$MT$SYM
-Icon=56C0_terminal.0
-StartupWMClass=terminal.exe
-EOF
-# Создание ярлыков на папки терминалов
-ln -s $MT$SYM $HOME/Desktop
- echo 'create desktop link ' $SYM ; 
-done
